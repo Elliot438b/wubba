bece1ce4c67e061e796af6eaa048312e1e1f821d
evsward <lw.wenbin@gmail.com>
2019-01-29 19:42:28 +0800

    vscode-test

--- rand/wubba.hpp
+++ rand/wubba.hpp
@@ -107,7 +107,7 @@ CONTRACT wubba : public contract
         {
             // equivalent to: hash = 65599*hash + (*str++);
             hash = (*str++) + (hash << 6) + (hash << 16) - hash;
-        }
+        } 
         return (hash & 0x7FFFFFFF);
     }
 
