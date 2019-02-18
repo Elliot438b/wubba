d9fa54451d9622bdab71a6df94eb670e620caab1
evsward <lw.wenbin@gmail.com>
2019-02-18 15:42:00 +0800

    demo::six card seeds.

--- tina/tina.cpp
+++ tina/tina.cpp
@@ -22,18 +22,25 @@ ACTION tina::erasingdata(uint64_t key)
 ACTION tina::testmultidex(asset money)
 {
     string seed = "10";
-    // eosio::print(" seed.substr(0,1)  : ", seed.substr(0, 1));
-    // eosio::print(" seed.substr(1,2)  : ", seed.substr(1, 2));
     eosio::checksum256 result = sha256(seed.c_str(), seed.size());
     eosio::print(" result : ", result);
     auto hash_data = result.extract_as_byte_array();
     string result_str = to_hex(reinterpret_cast<const char *>(hash_data.data()), 32);
-    eosio::print(" hash_data : ", result_str);
-    // auto counter = 0;
-    // for (auto w_itr = hash_data.begin(); w_itr != hash_data.end(); ++counter, ++w_itr)
-    // {
-    //     eosio::print(" ", counter, " : [", string((char *)w_itr), "]");
-    // }
+    std::vector<string> cardSeedsVec;
+    auto counter = 0;
+    while (counter < 6)
+    {
+        string temp_str = result_str.substr(counter * 9, 9);
+        // eosio::print(" counter : ", counter, " temp_str=", temp_str);
+        cardSeedsVec.emplace_back(temp_str);
+        counter++;
+    }
+
+    for (const auto &tem : cardSeedsVec)
+    {
+        eosio::print(" card seed =", tem);
+    }
+
     // eosio::print(" testtransfer : ", money);
     // INLINE_ACTION_SENDER(eosio::token, transfer)
     // (
